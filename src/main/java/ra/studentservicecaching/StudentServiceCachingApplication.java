da2bf2f4fde3d039f3b78508a660a760a47c1b7f
Tamisimi <alexmd0107@gmail.com>
2026-09-25 00:01:11 +0700

    Add main application class

---
+++ src/main/java/ra/studentservicecaching/StudentServiceCachingApplication.java
@@ -0,0 +1,13 @@
+package ra.studentservicecaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentServiceCachingApplication.class, args);
+    }
+
+}
